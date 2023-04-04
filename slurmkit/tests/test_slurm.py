b16241c8e4a436be74133c4854d2e49b8cba6157
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-04-04 14:38:47 -0700

    refactored slurm tests - removed shared_path fixture and switched to a monkey patch that changes the current working directory.

--- slurmkit/tests/test_slurm.py
+++ slurmkit/tests/test_slurm.py
@@ -18,36 +18,45 @@ def has_slurm() -> bool:
 pytestmark = pytest.mark.skipif(not has_slurm(), reason="SLURM not found.")
 
 
-@pytest.fixture
-def shared_path() -> Generator[Path, None, None]:
+@pytest.fixture(autouse=True)
+def change_to_shared_dir(monkeypatch: pytest.MonkeyPatch) -> Generator:
+    """Moves to temporary shared directory"""
+    prev_path = Path(".")
+
     path = Path(os.environ["MYDATA"])
     assert path.exists()
     path = path / "slurmkit_test"
+
     if path.exists():
         shutil.rmtree(path)
     path.mkdir()
-    yield path
+
+    monkeypatch.chdir(path)
+
+    yield
+
+    monkeypatch.chdir(prev_path)
     shutil.rmtree(path)
 
 
-def test_simple_slurm(shared_path: Path) -> None:
+def test_simple_slurm() -> None:
     @slurm_function
     def add(a: int, b: int) -> None:
         c = a + b
         print(f"{a} + {b} = {c}")
 
-    output = shared_path / "output-%j.out"
+    output = "output-%j.out"
     params = SlurmParams(output=output, mem="5M")
     job = submit_function(add(1, 2), params)
 
     # waiting for jobs to complete
     time.sleep(3)
 
-    with open(str(output).replace("%j", str(job))) as f:
+    with open(output.replace("%j", str(job))) as f:
         assert int(f.read()[-2]) == 3
 
 
-def test_slurm_dependency(shared_path: Path) -> None:
+def test_slurm_dependency() -> None:
     def _create_ones(size: int, out_path: Path) -> None:
         zarr.ones(shape=(size, size), store=zarr.DirectoryStore(out_path))
 
@@ -63,15 +72,15 @@ def test_slurm_dependency(shared_path: Path) -> None:
         out_arr[:] = in_arr1[:] + in_arr2[:]
 
     size = 5
-    params = SlurmParams(output=shared_path / "slurmkit-test-%j.out")
+    params = SlurmParams(output="slurmkit-test-%j.out")
 
     create_ones = slurm_function(_create_ones)(size=size)
 
-    paths = [shared_path / f"{i}.zarr" for i in range(2)]
+    paths = [f"{i}.zarr" for i in range(2)]
 
     jobs = [cast(int, submit_function(create_ones, params, out_path=p)) for p in paths]
 
-    out_path = shared_path / "out.zarr"
+    out_path = "out.zarr"
     submit_function(_sum_zarr(*paths, out_path=out_path), params, dependencies=jobs)
 
     # waiting for jobs to complete
@@ -81,13 +90,13 @@ def test_slurm_dependency(shared_path: Path) -> None:
     assert np.allclose(arr[:], 2)
 
 
-def test_int_indexing(shared_path: Path) -> None:
+def test_int_indexing() -> None:
     @slurm_function
     def _show_int(value: int) -> None:
         assert isinstance(value, int)
         print(f"Value {size}", value)
 
-    output = shared_path / "slurmkit-test-%j.out"
+    output = "slurmkit-test-%j.out"
     params = SlurmParams(output=output)
     size = 10
 
